7dfe22c80aa0ffb6df9151f1adc53ae33fdc1389
MJX.SH <mrityunjay.209302233@muj.manipal.edu>
2023-11-23 23:58:59 +0530

    final version

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# ❄️ Winterly Tab ❄️
+# ☀️ Summerly Tab ☀️
 <!-- ALL-CONTRIBUTORS-BADGE:START - Do not remove or modify this section -->
 [![All Contributors](https://img.shields.io/badge/all_contributors-8-orange.svg?style=flat-square)](#contributors-)
 <!-- ALL-CONTRIBUTORS-BADGE:END -->
--- src/util/index.js
+++ src/util/index.js
@@ -1,11 +1,16 @@
 import axios from "axios";
 
 export const retrieveImage = async () => {
-  let image = await axios.get("https://qolqi.sse.codesandbox.io/images");
-  const totalImage = image.data.length;
-  const min = Math.ceil(0);
-  const max = Math.floor(totalImage);
-  const n = Math.floor(Math.random() * (max - min + 1)) + min;
-  image = image.data[n];
-  return image;
+  let image = await axios.get("https://api.unsplash.com/photos/random?collections=3403106,3174571&content_filter=high",{
+    headers: {
+      Authorization:"Client-ID HZUbRLoQXpv28VMYdx1AyHymycZBsJJ9g2rQODcHBDA",
+    }
+  });
+  console.log(image.data)
+  // const totalImage = image.data.length;
+  // const min = Math.ceil(0);
+  // const max = Math.floor(totalImage);
+  // const n = Math.floor(Math.random() * (max - min + 1)) + min;
+  // image = image.data[n];
+  return image.data;
 };
